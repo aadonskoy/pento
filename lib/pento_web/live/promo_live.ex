c8dbfa0bf55f19ba3b5abdff1f24d0a86f9fd73c
Oleksii Donskoi <aadonskoy@gmail.com>
2022-11-07 14:51:31 +0200

    Adds schemaless promo

---
+++ lib/pento_web/live/promo_live.ex
@@ -0,0 +1,65 @@
+defmodule PentoWeb.PromoLive do
+  use PentoWeb, :live_view
+  alias Pento.Promo
+  alias Pento.Promo.Recipient
+
+  def mount(_params, _session, socket) do
+    {:ok,
+      socket
+      |> assign_recipient()
+      |> assign_changeset()}
+  end
+
+  def handle_event(
+    "validate",
+    %{"recipient" => recipient_params},
+    %{assigns: %{recipient: recipient}} = socket
+  ) do
+    changeset =
+      recipient
+      |> Promo.change_recipient(recipient_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply,
+    socket
+    |> put_flash(:error, nil)
+    |> assign(:changeset, changeset)}
+  end
+
+  def handle_event(
+    "save",
+    %{"recipient" => recipient_params},
+    %{assigns: %{recipient: recipient}} = socket
+  ) do
+    changeset =
+      recipient
+      |> Promo.change_recipient(recipient_params)
+      |> Map.put(:action, :validate)
+
+
+    if changeset.valid? do
+      Promo.send_promo(recipient, recipient_params)
+
+      {:noreply,
+      socket
+      |> put_flash(:info, "Promo has been sent")
+      |> assign_recipient()
+      |> assign_changeset()}
+    else
+      {:noreply,
+      socket
+      |> put_flash(:error, "Error with sending")
+      |> assign(:changeset, changeset)}
+    end
+  end
+
+  def assign_recipient(socket) do
+    socket
+    |> assign(:recipient, %Recipient{})
+  end
+
+  def assign_changeset(%{assigns: %{recipient: recipient}} = socket) do
+    socket
+    |> assign(:changeset, Promo.change_recipient(recipient))
+  end
+end
